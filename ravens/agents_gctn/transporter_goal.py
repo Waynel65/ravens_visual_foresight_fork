46bec7e26f4d2dfeafaed828b5720ebf56a0bab4
Waynel65 <waynel@bu.edu>
2023-08-29 16:08:04 -0500

    added pdb in test tvf

--- ravens/agents_gctn/transporter_goal.py
+++ ravens/agents_gctn/transporter_goal.py
@@ -282,6 +282,7 @@ class TransporterAgent:
             attention = self.attention_model.forward(input_only)
         else:
             attention = self.attention_model.forward(input_image)
+        pdb.set_trace()
         argmax = np.argmax(attention)
         argmax = np.unravel_index(argmax, shape=attention.shape)
         p0_pixel = argmax[:2]
